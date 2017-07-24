5fcf1e535bca00a9862da129aa5410c9904f6f70
njxmi <nicole.jones.mi@gmail.com>
2017-07-24 08:52:27 -0400

    First commit, wasn't able to finish correcting code by deadline

---
+++ script.js
@@ -0,0 +1,77 @@
+script for circle
+
+// area of a circle is 22/7 * r*r
+// pick an element to scale relative to size of circle
+//display "The area of a cricle with radius r is X sq units."
+
+
+
+function calcArea() {
+    var x = document.getElementById("input").form.id;
+	var radius =document.form1.txtRadius.value;
+        document.write("The area of the circle with a radius of " x + " ix" + (radius * radius * Math.PI));
+    document.getElementById("numberForm").innerHTML = x;
+	btn.appendChild(t);
+}
+
+function CalculateArea(){
+        var radius =document.form1.txtRadius.value;
+        document.write("The area of the circle is " + (radius * radius * Math.PI) + "</p>");
+       
+    }
+
+	//96 pixels is appx 1 inch
+	
+if (radius === 1) {
+//If input is 1 return image mercury
+picture = (image info here);
+}
+else if (radius = 2) {
+//If input in range of ??? return image  mars
+picture = (image info here);
+}
+//If input in range of ???? return image venus
+else if (radius = 3) {
+//If input in range of ??? return image  mars
+picture = (image info here);
+}
+//If input in range of ???? return image earth
+else if (radius = 4) {
+//If input in range of ??? return image  mars
+picture = (image info here);
+}
+//If input in range of ???? return image neptune
+else if (radius = 5) {
+//If input in range of ??? return image  mars
+picture = (image info here);
+}
+//If input in range of ???? return image uranus
+else if (radius = 6) {
+//If input in range of ??? return image  mars
+picture = (image info here);
+}
+//If input in range of ???? return image saturn
+else if (radius = 7) {
+//If input in range of ??? return image  mars
+picture = (image info here);
+}
+//If input in range of ???? return image 
+else {
+//If input in range of ??? return image  mars
+picture = (image info here);
+}
+
+
+
+/*
+
+    Mercury   —  ⌀   4.879,4 km  —    38% the size of Earth   
+    Mars      —  ⌀   6.779 km    —    53% the size of Earth   
+    Venus     —  ⌀  12.104 km    —    95% the size of Earth   
+    Earth     —  ⌀  12.756 km    —   100% the size of Earth   
+    Neptune   —  ⌀  49.528 km    —   388% the size of Earth  
+    Uranus    —  ⌀  51.118 km    —   400% the size of Earth  
+    Saturn    —  ⌀ 120.660 km    —   945% the size of Earth  
+    Jupiter   —  ⌀ 142.800 km    —  1120% the size of Earth 
+	
+	*/
